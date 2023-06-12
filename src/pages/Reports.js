b4a42df084724ba7becc35c6d6cd38be5af109dc
QudsiaZainab <95419016+QudsiaZainab@users.noreply.github.com>
2023-06-12 17:43:11 +0500

    Add files via upload

---
+++ src/pages/Reports.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Reports() {
+  return (
+    <div>
+      <ul>
+        <li>Qudsia</li>
+        <li>Hala</li>
+        <li>Afifa</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Reports;
